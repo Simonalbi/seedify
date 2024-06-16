6babfabe7901d945ebf0532ae64454c071efa3c3
Valentina Ferrentino <v.ferrentino10@studenti.unisa.it>
2024-06-16 17:21:58 +0200

    Minor.

--- src/main/java/com/unisa/seedify/model/UserBean.java
+++ src/main/java/com/unisa/seedify/model/UserBean.java
@@ -20,7 +20,7 @@ public class UserBean implements Serializable {
         }
     }
 
-    private static final long serialVersionID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private String email;
     private String password;
--- src/main/java/com/unisa/seedify/model/UserDao.java
+++ src/main/java/com/unisa/seedify/model/UserDao.java
@@ -8,31 +8,34 @@ public class UserDao extends BaseDao implements GenericDao<UserBean> {
     private static final String TABLE_NAME = "utenti";
 
     @Override
-    public synchronized void doSave(UserBean user) throws SQLException {
-        String query = "INSERT INTO " + UserDao.TABLE_NAME + " (email, password, foto_profilo, nome, cognome, ruolo) VALUES (?, ?, ?, ?, ?, ?)";
+    public synchronized void doSave(UserBean userBean) throws SQLException {
+        String query = "INSERT INTO " + UserDao.TABLE_NAME +
+                       " (email, password, foto_profilo, nome, cognome, ruolo)" +
+                       " VALUES (?, ?, ?, ?, ?, ?)";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
-            preparedStatement.setString(1, user.getEmail());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.setBytes(3, user.getProfilePicture());
-            preparedStatement.setString(4, user.getName());
-            preparedStatement.setString(5, user.getSurname());
-            preparedStatement.setString(6, user.getRole().toString());
+            preparedStatement.setString(1, userBean.getEmail());
+            preparedStatement.setString(2, userBean.getPassword());
+            preparedStatement.setBytes(3, userBean.getProfilePicture());
+            preparedStatement.setString(4, userBean.getName());
+            preparedStatement.setString(5, userBean.getSurname());
+            preparedStatement.setString(6, userBean.getRole().toString());
 
             preparedStatement.executeUpdate();
         }
     }
 
     @Override
-    public synchronized void doDelete(UserBean user) throws SQLException {
-        String query = "DELETE FROM " + UserDao.TABLE_NAME + " WHERE email = ?";
+    public synchronized void doDelete(UserBean userBean) throws SQLException {
+        String query = "DELETE FROM " + UserDao.TABLE_NAME +
+                       " WHERE email = ?";
 
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
-            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setString(1, userBean.getEmail());
 
             preparedStatement.executeUpdate();
         }
