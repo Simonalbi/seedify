f94e6873a8f2d2ee522b84cc083f3dde30c5ebb7
Valentina Ferrentino <v.ferrentino10@studenti.unisa.it>
2024-06-25 10:38:00 +0200

    Added session and redirect logic

--- src/main/java/com/unisa/seedify/control/LoginServlet.java
+++ src/main/java/com/unisa/seedify/control/LoginServlet.java
@@ -5,24 +5,39 @@ import com.unisa.seedify.model.EntityPrimaryKey;
 import com.unisa.seedify.model.UserBean;
 import com.unisa.seedify.model.UserDao;
 
-import javax.imageio.ImageIO;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.Console;
-import java.io.File;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.UUID;
 
 // TODO Ajax per inviare i messaggi di errore al client
+// TODO: Get role from user and redirect to correct page
 @WebServlet(name = "loginServlet", value = "/login-servlet")
 public class LoginServlet extends HttpServlet {
     private static final UserDao userDao = UserDao.getInstance();
 
     public void init() {
+    }
+
+    // TODO Redirect to correct role page
+    private void redirectUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + "REDIRECT_PAGE");
+        dispatcher.forward(request, response);
+    }
+
+    private void initSession(HttpServletRequest request, HttpServletResponse response, UserBean user) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+
+        UUID uuid = UUID.randomUUID();
+        session.setAttribute("token", uuid.toString());
+
+        session.setAttribute("user", user);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -35,23 +50,37 @@ public class LoginServlet extends HttpServlet {
         if (!InputValidation.isPasswordStrong(password)) {
             throw new IllegalArgumentException("Password is not strong enough");
         }
+        password = InputValidation.sha256(password);
 
         EntityPrimaryKey userPrimaryKey = new EntityPrimaryKey();
         userPrimaryKey.addKey("email", email);
-        userPrimaryKey.addKey("password", InputValidation.sha256(password));
 
         UserBean user = null;
         try {
             user = userDao.doRetrive(userPrimaryKey);
-            // TODO: Get role from user and redirect to correct page
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
         if (user == null) {
             throw new IllegalArgumentException("Invalid email or password");
+        } else if (user.getPassword().equals(password)) {
+            try {
+                this.initSession(request, response, user);
+                this.redirectUser(request, response);
+            } catch (ServletException e) {
+                RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/common/errors/404.jsp");
+                try {
+                    dispatcher.forward(request, response);
+                } catch (ServletException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid email or password");
         }
 
+        // DUBUG
         System.out.println("User " + user.getEmail() + " logged in");
     }
 
